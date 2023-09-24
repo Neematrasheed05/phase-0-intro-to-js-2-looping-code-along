3a7ff7ee3dff3796931d2c2b3f601a9cf7ba8bbc
Neematrashid <nymatrasheed05@gmail.com>
2023-09-24 19:52:28 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, gift){
+    let thankYouMessage = [];
+    for (let name of names){
+        let message = `Thank you, ${name}, for the wonderful ${gift} gift!`;
+        thankYouMessage.push(message);
+    }
+    return thankYouMessage;
+}
+
+function countDown(){
+    let no = 10;
+while (no >= 0){
+    console.log(no);
+    no--;
+} 
+}
+countDown();
